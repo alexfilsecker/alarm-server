119b288529d6d6d685c8a6226a1e634f563c5050
Alex <afilsecker@u-zave.com>
2023-12-08 13:43:35 -0300

    get and post

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,19 @@ const app = express();
 app.use(cors({ origin: true }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  console.log(req.method, req.path);
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.post('/', (req, res) => {
+  const { body } = req;
+  console.log(body);
+  return res.send('OK');
 });
 
 app.listen(8000, () => {

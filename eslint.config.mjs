2aedf4494779bb343643868b93afb37edbbd05bb
Alex <alexfilsecker@gmail.com>
2025-04-28 01:31:45 -0400

    eslint ignore _

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,18 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+    },
+  },
 );
